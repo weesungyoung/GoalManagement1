ffa7ebd9e9889d888c5caf5c547085f56fb9d3a4
weesungyoung <103415679+weesungyoung@users.noreply.github.com>
2024-10-03 17:20:11 +0900

    티켓 추가에 대한 메인 페이지 카드 동적 생성

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,31 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.JwtUtil;
+import com.example.demo.model.Ticket;
+import com.example.demo.service.TicketService;
+import com.example.demo.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 
 @Controller
 public class HomeController {
+
+    private final TicketService ticketService;
+
+    @Autowired
+    public HomeController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
 
     @GetMapping("/")
     public String showHome(HttpSession session, Model model) {
@@ -17,7 +34,11 @@ public class HomeController {
         Integer adminStatus = (Integer) session.getAttribute("isAdmin");
         boolean isAdmin = (adminStatus != null) && (adminStatus == 1);
 
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date currentDate = new Date();
+        List<Ticket> tickets = ticketService.getTicketByEndDate(dateFormat.format(currentDate));
 
+        model.addAttribute("ticket", tickets);
         model.addAttribute("isLoggedIn", isLoggedIn);
         model.addAttribute("isAdmin", isAdmin);
         return "index";
